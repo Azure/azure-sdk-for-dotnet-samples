ba58c09a72c16d0aceaf7b662ae220a4ff693a47
tg-msft <Ted.Glaza@microsoft.com>
2019-09-18 11:13:27 -0700

    Initial version of CloudClipboard

---
+++ CloudClipboard/CloudClipboard/Pages/Index.cshtml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#region v11 usings
+// using Microsoft.Azure.Storage;
+// using Microsoft.Azure.Storage.Blob;
+#endregion
+
+
+namespace CloudClipboard.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string UserId => (string)HttpContext.Items["UserId"];
+
+
+        public BlobServiceClient ClipsBlobService { get; }
+
+        public IndexModel(BlobServiceClient clipService)
+        {
+            ClipsBlobService = clipService;
+        }
+
+
+        public List<string> ClipIds { get; set; } = new List<string>();
+
+        public async Task OnGetAsync()
+        {
+            BlobContainerClient userContainer = ClipsBlobService.GetBlobContainerClient(UserId);
+            await foreach (BlobItem clipBlob in userContainer.GetBlobsAsync())
+            {
+                ClipIds.Add(clipBlob.Name);
+            }
+
+            #region v11 ListBlobsSegmentedAsync
+            // CloudBlobContainer userContainer = ClipsBlobService.GetContainerReference(UserId);
+            // BlobContinuationToken continuation = null;
+            // do
+            // {
+            //     BlobResultSegment segment = await userContainer.ListBlobsSegmentedAsync(null, continuation);
+            //     continuation = segment.ContinuationToken;
+            //     foreach (IListBlobItem clipBlob in segment.Results)
+            //     {
+            //         ClipIds.Add(new BlobUriBuilder(clipBlob.Uri).BlobName);
+            //     }
+            // } while (continuation != null);
+            #endregion
+        }
+
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // Upload the new clip as a blob
+            using var stream = GetNewClipAsStream();
+            string name = Guid.NewGuid().ToString();
+
+            BlobClient blob = ClipsBlobService.GetBlobContainerClient(UserId).GetBlobClient(name);
+            await blob.UploadAsync(stream);
+
+            #region v11 UploadFromStreamAsync
+            // CloudBlockBlob blob = ClipsBlobService.GetContainerReference(UserId).GetBlockBlobReference(name);
+            // await blob.UploadFromStreamAsync(stream);
+            #endregion
+
+            // Navigate to that page
+            return RedirectToPage("Clip", new { userId = this.UserId, clipId = name });
+        }
+
+        [BindProperty]
+        public string NewClip { get; set; }
+
+        public Stream GetNewClipAsStream()
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(NewClip));
+        }
+    }
+}
---
+++ CloudClipboard/GarbageCollector/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace GarbageCollector
+{
+    public static class Program
+    {
+        private static readonly Uri BlobServiceUri = new Uri("https://cloudclips.blob.core.windows.net/");
+
+        public static async Task Main()
+        {
+            BlobClientOptions options = new BlobClientOptions
+            {
+                Retry =
+                {
+                    MaxRetries = 10,
+                    Delay = TimeSpan.FromSeconds(3)
+                },
+                Diagnostics =
+                {
+                    IsLoggingEnabled = true
+                }
+            };
+
+            options.AddPolicy(HttpPipelinePosition.PerCall, new SimpleTracingPolicy());
+
+
+            BlobServiceClient serviceClient = new BlobServiceClient(BlobServiceUri, new DefaultAzureCredential(), options);
+            await foreach (ContainerItem container in serviceClient.GetContainersAsync())
+            {
+                await serviceClient.GetBlobContainerClient(container.Name).DeleteAsync();
+            }
+        }
+    }
+
+    public class SimpleTracingPolicy : HttpPipelinePolicy
+    {
+        public override async Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            Console.WriteLine($">> Request: {message.Request.Method} {message.Request.UriBuilder.Uri}");
+            await ProcessNextAsync(message, pipeline);
+            Console.WriteLine($">> Response: {message.Response.Status} from {message.Request.Method} {message.Request.UriBuilder.Uri}");
+        }
+
+        #region public override void Process
+        public override void Process(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            Console.WriteLine($">> Request: {message.Request.UriBuilder.Uri}");
+            ProcessNext(message, pipeline);
+            Console.WriteLine($">> Response: {message.Response.Status} {message.Request.Method} {message.Request.UriBuilder.Uri}");
+        }
+        #endregion public override void Process
+    }
+}
